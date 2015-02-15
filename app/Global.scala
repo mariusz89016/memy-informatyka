cbb3bdd4eadb1b51369d9d90908972c4e43742fe
mariusz89016 <mariusz89016@gmail.com>
2015-02-15 20:09:50 +0100

    Creating table in database only when not exists

--- app/Global.scala
+++ app/Global.scala
@@ -5,25 +5,21 @@ import play.api.Play.current
 import play.api.db.DB
 import scala.slick.driver.PostgresDriver.simple._
 import models.Memes
+import scala.slick.jdbc.meta.MTable
 
 object Global extends GlobalSettings {
   val memes = TableQuery[Memes]
   lazy val database = Database.forDataSource(DB.getDataSource())
   override def onStart(app: Application): Unit = {
     database.withSession { implicit session =>
-      memes.ddl.create
+      if (MTable.getTables("MEMES").list.isEmpty) {
+        memes.ddl.create
 
-      memes += Mem(1, "nazwa1", "opis1", "autor1")
-      memes += Mem(2, "nazwa2", "opis2", "autor2")
-      memes += Mem(3, "nazwa3", "opis3", "autor3")
-      memes += Mem(4, "nazwa4", "opis4", "autor4")
+        memes += Mem(1, "nazwa1", "opis1", "autor1")
+        memes += Mem(2, "nazwa2", "opis2", "autor2")
+        memes += Mem(3, "nazwa3", "opis3", "autor3")
+        memes += Mem(4, "nazwa4", "opis4", "autor4")
+      }
     }
   }
-
-  override def onStop(app: Application): Unit = {
-    database.withSession { implicit session =>
-      memes.ddl.drop
-    }
-  }
-
 }
